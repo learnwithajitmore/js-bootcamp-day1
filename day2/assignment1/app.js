407a4463b7e2ed3c4f433337591fd7a9e4adfa36
learnwithajitmore <146984959+learnwithajitmore@users.noreply.github.com>
2023-10-06 00:44:39 +0530

    Delete day2 directory

--- day2/assignment1/app.js
+++
@@ -1,8 +0,0 @@
-function getRandomColor(){
-    const randomColor = Math.floor(Math.random()*16777215).toString(16);
-    return "#" + randomColor;
-}
-
-function changeBackgroundColor(){
-    document.body.style.backgroundColor = getRandomColor();
-}
