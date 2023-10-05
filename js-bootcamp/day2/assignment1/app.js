fadf912eff02ec8157fc13234e7258f75be1ced8
learnwithajitmore <146984959+learnwithajitmore@users.noreply.github.com>
2023-10-06 00:45:43 +0530

    Add files via upload

---
+++ js-bootcamp/day2/assignment1/app.js
@@ -0,0 +1,8 @@
+function getRandomColor(){
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    return "#" + randomColor;
+}
+
+function changeBackgroundColor(){
+    document.body.style.backgroundColor = getRandomColor();
+}
